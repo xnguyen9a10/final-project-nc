64a647ceabc740a1a26b3495e89bbf6b32501222
tumoqun <32707186+tumoqun@users.noreply.github.com>
2020-08-03 14:12:56 +0700

    fix app.js

--- client/src/components/customer/App.js
+++ client/src/components/customer/App.js
@@ -17,20 +17,20 @@ import history from "../../utils/history";
 import Profile from './Profile';
 import { useForm } from "react-hook-form";
 import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-    Switch,
-    useParams,
-    HashRouter,
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useParams,
+  HashRouter,
 } from 'react-router-dom'
 import {
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-    MoneyCollectOutlined,
-    HistoryOutlined,
-    LoginOutlined
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  MoneyCollectOutlined,
+  HistoryOutlined,
+  LoginOutlined
 } from "@ant-design/icons";
 import FundTransfer from './FundTransfer';
 import ChangePassword from './ChangePassword'
@@ -38,190 +38,195 @@ import transactionHistory from "./transactionHistory";
 import accountHistory from './accountHistory';
 import outside from './outsideTranfer';
 import Debs from './debs';
+import ReceiversManagement from "./receiversManagement"
 
 function App(props) {
-    const { Header, Content, Sider } = Layout;
+  const { Header, Content, Sider } = Layout;
 
-    return (
-      <Router history={history}>
-        <Layout style={{ height: "100vh" }}>
-          <Sider
-            id="sider"
-            breakpoint="lg"
-            collapsedWidth="0"
-            onBreakpoint={(broken) => {
-              console.log(broken);
-            }}
-            onCollapse={(collapsed, type) => {
-              console.log(collapsed, type);
-            }}
+  return (
+    <Router history={history}>
+      <Layout style={{ height: "100vh" }}>
+        <Sider
+          id="sider"
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="logo" />
+
+          <Menu
+            id="menu"
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
           >
-            <div className="logo" />
-
+            <Menu.Item
+              key="1"
+              icon={<UserOutlined style={{ marginTop: "10px;" }} />}
+            >
+              <Link to="/">TRANG CHỦ</Link>
+            </Menu.Item>
+            <Menu.Item
+              id="drop_menu"
+              key="2"
+              icon={<MoneyCollectOutlined />}
+              onClick={() => {
+                document.getElementById("submenu").style.display = "block";
+                document.getElementById("sider").style.minWidth = "250px";
+                document.getElementById("sider").style.maxWidth = "250px";
+                if (document.getElementById("sub_content") != null) {
+                  document.getElementById("sub_content").style.display =
+                    "none";
+                }
+              }}
+            >
+              <Link
+                to="/customer/services" /*onClick={(e)=>e.preventDefault()}*/
+              >
+                DỊCH VỤ
+                </Link>
+            </Menu.Item>
             <Menu
-              id="menu"
+              id="submenu"
+              defaultSelectedKeys={['21']}
+              style={{
+                borderTop: "0.1px solid lightgrey",
+                borderBottom: "0.1px solid lightgrey",
+                display: "none",
+              }}
               theme="dark"
               mode="inline"
-              defaultSelectedKeys={["1"]}
             >
-              <Menu.Item
-                key="1"
-                icon={<UserOutlined style={{ marginTop: "10px;" }} />}
-              >
-                <Link to="/">TRANG CHỦ</Link>
-              </Menu.Item>
-              <Menu.Item
-                id="drop_menu"
-                key="2"
-                icon={<MoneyCollectOutlined />}
-                onClick={() => {
-                  document.getElementById("submenu").style.display = "block";
-                  document.getElementById("sider").style.minWidth = "250px";
-                  document.getElementById("sider").style.maxWidth = "250px";
-                  if (document.getElementById("sub_content") != null) {
-                    document.getElementById("sub_content").style.display =
-                      "none";
-                  }
-                }}
-              >
+              <Menu.Item key="21">
                 <Link
-                  to="/customer/services" /*onClick={(e)=>e.preventDefault()}*/
+                  to="/customer/profile"
+                  style={{ paddingLeft: "20px" }}
+                  onClick={(e) => {
+                    var el = document.getElementById("menu");
+                    for (var i = 0; i < el.length; i++) {
+                      el[i].classList.remove("ant-menu-item-selected");
+                    }
+                    var el = document.getElementById("submenu");
+                    for (var i = 0; i < el.length; i++) {
+                      el[i].classList.remove("ant-menu-item-selected");
+                    }
+                    e.target.parentElement.classList.add(
+                      "ant-menu-item-selected"
+                    );
+                  }}
                 >
-                  DỊCH VỤ
-                </Link>
+                  Thông tin tài khoản
+                  </Link>
               </Menu.Item>
-              <Menu
-                id="submenu"
-                defaultSelectedKeys={['21']}
-                style={{
-                  borderTop: "0.1px solid lightgrey",
-                  borderBottom: "0.1px solid lightgrey",
-                  display: "none",
-                }}
-                theme="dark"
-                mode="inline"
-              >
-                <Menu.Item key="21">
-                  <Link
-                    to="/customer/profile"
-                    style={{ paddingLeft: "20px" }}
-                    onClick={(e) => {
-                      var el = document.getElementById("menu");
-                      for (var i = 0; i < el.length; i++) {
-                        el[i].classList.remove("ant-menu-item-selected");
-                      }
-                      var el = document.getElementById("submenu");
-                      for (var i = 0; i < el.length; i++) {
-                        el[i].classList.remove("ant-menu-item-selected");
-                      }
-                      e.target.parentElement.classList.add(
-                        "ant-menu-item-selected"
-                      );
-                    }}
-                  >
-                    Thông tin tài khoản
+              <Menu.Item key="22">
+                <Link
+                  to="/customer/fund-transfer"
+                  style={{ paddingLeft: "20px" }}
+                >
+                  Thanh toán
                   </Link>
-                </Menu.Item>
-                <Menu.Item key="22">
-                  <Link
-                    to="/customer/fund-transfer"
-                    style={{ paddingLeft: "20px" }}
-                  >
-                    Thanh toán
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="23">
-                  <Link
-                    to="/customer/change-password"
-                    style={{ paddingLeft: "20px" }}
-                  >
-                    Đổi mật khẩu
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="25" icon={<HistoryOutlined />}>
-                  <Link to="/customer/transfer">
-                    Chuyển khoản ngoài hệ thống
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="26">
-                  <Link to="/customer/debs" style={{ paddingLeft: "20px" }}>
-                    Quản lý nhắc nợ
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="24">
-                  <Link
-                    to="/customer/transaction-history"
-                    style={{ paddingLeft: "20px" }}
-                  >
-                    Quản lý tài khoản
-                  </Link>
-                </Menu.Item>
-              </Menu>
-              <Menu.Item key="3" icon={<HistoryOutlined />}>
-                <Link to="/employee/transaction-history">LIÊN HỆ</Link>
               </Menu.Item>
-              <Menu.Item
-                key="4"
-                icon={<LoginOutlined />}
-                onClick={() => {
-                  localStorage.clear();
-                  window.location.replace(`/login`);
-                }}
-              >
-                Đăng xuất
+              <Menu.Item key="23">
+                <Link
+                  to="/customer/change-password"
+                  style={{ paddingLeft: "20px" }}
+                >
+                  Đổi mật khẩu
+                  </Link>
+              </Menu.Item>
+              <Menu.Item key="27">
+                <Link to="/customer/receivers-management" style={{ paddingLeft: "20px" }}>Quản lý danh sách người nhận</Link>
+              </Menu.Item>
+              <Menu.Item key="25" icon={<HistoryOutlined />}>
+                <Link to="/customer/transfer">
+                  Chuyển khoản ngoài hệ thống
+                  </Link>
+              </Menu.Item>
+              <Menu.Item key="26">
+                <Link to="/customer/debs" style={{ paddingLeft: "20px" }}>
+                  Quản lý nhắc nợ
+                  </Link>
+              </Menu.Item>
+              <Menu.Item key="24">
+                <Link
+                  to="/customer/transaction-history"
+                  style={{ paddingLeft: "20px" }}
+                >
+                  Quản lý tài khoản
+                  </Link>
               </Menu.Item>
             </Menu>
-          </Sider>
+            <Menu.Item key="3" icon={<HistoryOutlined />}>
+              <Link to="/employee/transaction-history">LIÊN HỆ</Link>
+            </Menu.Item>
+            <Menu.Item
+              key="4"
+              icon={<LoginOutlined />}
+              onClick={() => {
+                localStorage.clear();
+                window.location.replace(`/login`);
+              }}
+            >
+              Đăng xuất
+              </Menu.Item>
+          </Menu>
+        </Sider>
 
-          <Layout>
-            {/* <Header className="site-layout-sub-header-background" style={{ backgroundColor: "#00008B" }}>
+        <Layout>
+          {/* <Header className="site-layout-sub-header-background" style={{ backgroundColor: "#00008B" }}>
                         <div style={{ textAlign: "center", color: "white", fontSize: "28px" }}>
                             <span><img src={profile_icon} height="50px" style={{ paddingRight: "10px" }} /></span>
                     Thông tin tài khoản
                     </div>
                     </Header> */}
-            <Switch>
-              <Route
-                path="/customer/services"
-                render={(props) => <Service props />}
-              ></Route>
-              <Route path="/customer/profile">{<Profile props />}</Route>
-              <Route path="/customer/fund-transfer" component={FundTransfer} />
-              <Route
-                path="/customer/change-password"
-                component={ChangePassword}
-              />
-              <Route
-                path="/customer/transaction-history"
-                component={transactionHistory}
-              />
-              <Route
-                path="/customer/accountHistory/:accountId"
-                component={accountHistory}
-              />
-              <Route path="/customer/transfer" component={outside} />
-              <Route path="/customer/debs" component={Debs} />
-              <Route exact path="/customer" component={Home} />
-            </Switch>
-          </Layout>
+          <Switch>
+            <Route
+              path="/customer/services"
+              render={(props) => <Service props />}
+            ></Route>
+            <Route path="/customer/profile">{<Profile props />}</Route>
+            <Route path="/customer/fund-transfer" component={FundTransfer} />
+            <Route path="/customer/receivers-management" component={ReceiversManagement}/>
+            <Route
+              path="/customer/change-password"
+              component={ChangePassword}
+            />
+            <Route
+              path="/customer/transaction-history"
+              component={transactionHistory}
+            />
+            <Route
+              path="/customer/accountHistory/:accountId"
+              component={accountHistory}
+            />
+            <Route path="/customer/transfer" component={outside} />
+            <Route path="/customer/debs" component={Debs} />
+            <Route exact path="/customer" component={Home} />
+          </Switch>
         </Layout>
-      </Router>
-    );
+      </Layout>
+    </Router>
+  );
 }
 
 function Service() {
-    function click() {
-        document.getElementById("list").style.display = "none";
+  function click() {
+    document.getElementById("list").style.display = "none";
+  }
+  if (document.getElementById("list") != null) {
+    if (document.getElementById("list").style.display == "none") {
+      document.getElementById("list").style.display = "block";
     }
-    if (document.getElementById("list") != null) {
-        if (document.getElementById("list").style.display == "none") {
-            document.getElementById("list").style.display = "block";
-        }
-    }
-    return (
-        <Router>
-            <div className="site-layout-background" style={{ padding: 24, height: "600px" }}>
-                {/* <ul id="list" style={{ display: 'block' }}>
+  }
+  return (
+    <Router>
+      <div className="site-layout-background" style={{ padding: 24, height: "600px" }}>
+        {/* <ul id="list" style={{ display: 'block' }}>
                     <li style={{ display: 'inline-block', width: '25%', paddingLeft: "24px", paddingRight: "24px" }}>
                         <Link to="/customer/profile" style={{ textDecoration: "none" }} onClick={() => {
                             document.getElementById("list").style.display = "none";
@@ -327,23 +332,23 @@ function Service() {
                     </li>
                 </ul> */}
 
-                <Switch>
-                    <Route path="/customer/profile" component={Profile} />
-                    <Route path="/customer/fund-transfer" component={FundTransfer} />
-                    <Route path="/customer/change-password" component={ChangePassword} />
-                    <Route exact path="/customer" component={Service} />
-                </Switch>
+        <Switch>
+          <Route path="/customer/profile" component={Profile} />
+          <Route path="/customer/fund-transfer" component={FundTransfer} />
+          <Route path="/customer/change-password" component={ChangePassword} />
+          <Route exact path="/customer" component={Service} />
+        </Switch>
 
-            </div>
-        </Router>
-    )
+      </div>
+    </Router>
+  )
 }
 
 
 function Home(props) {
-    return (
-        <div>This is Home</div>
-    )
+  return (
+    <div>This is Home</div>
+  )
 }
 
 export default App;
